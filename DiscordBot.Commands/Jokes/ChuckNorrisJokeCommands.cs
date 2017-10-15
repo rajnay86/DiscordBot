7797c96f0c326eb25dd53e881f74f5a49c198922
rajnay86@gmail.com <rajnay86@gmail.com>
2017-10-14 21:59:20 -0400

    Add joke commands

---
+++ DiscordBot.Commands/Jokes/ChuckNorrisJokeCommands.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands.Jokes
+{
+
+    public class ChuckNorrisJokeResult<T>
+    {
+        public string type { get; set; }
+        public T value { get; set; }
+    }
+
+    public class ChuckNorrisJoke
+    {
+        public int id { get; set; }
+        public string joke { get; set; }
+        public List<string> categories { get; set; }
+    }
+
+    public class ChuckNorrisJokeCommands
+    {
+        [Command("chucknorrisjoke")]
+        public async Task GetChuckNorrisJoke(CommandContext ctx)
+        {
+            var client = new RestClient("http://api.icndb.com/jokes");
+            var request = new RestRequest("random");
+            var response = client.Execute<ChuckNorrisJokeResult<ChuckNorrisJoke>>(request);
+            var joke = response.Data.value;
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync(joke.joke);
+        }
+    }
+}
---
+++ DiscordBot.Commands/Jokes/DadJokeCommands.cs
@@ -0,0 +1,30 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using RestSharp;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands.Jokes
+{
+    public class DadJoke
+    {
+        public string id { get; set; }
+        public string joke { get; set; }
+        public int status { get; set; }
+    }
+
+    public class JokeCommands
+    {
+        [Command("dadjoke")]
+        public async Task GetDadJoke(CommandContext ctx)
+        {
+            var client = new RestClient("https://icanhazdadjoke.com");
+            var request = new RestRequest();
+            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+            var response = client.Execute< DadJoke>(request);
+            var dadJoke = response.Data;
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync(dadJoke.joke);
+        }
+    }
+}
