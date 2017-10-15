7fa8a6cefd72c7ec445d378a24a2d1f3b5df5353
rajnay86@gmail.com <rajnay86@gmail.com>
2017-10-14 21:58:54 -0400

    Add gw2 wallet commands

---
+++ DiscordBot.Commands/GW2/Account/WalletCommands.cs
@@ -0,0 +1,35 @@
+﻿using DiscordBot.Commands.GW2.Authentication;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using GW2API.V2.Authenticated.Account.Repository;
+using GW2API.V2.Misc.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands.GW2.Account
+{
+    public class WalletCommands
+    {
+        [Command("get-wallet")]
+        public async Task GetWallet(CommandContext ctx)
+        {
+            var apiKey = DatabaseAccess.GetApiKey(ctx.User.Id);
+            WalletRepository walletRepo = new WalletRepository(apiKey);
+            CurrencyRepository currencyRepo = new CurrencyRepository();
+            var wallet = walletRepo.GetAllItems().Result;
+            var currencies = currencyRepo.GetAllItems().Result;
+            List<string> walletItems = new List<string>();
+            foreach(var walletItem in wallet)
+            {
+                var name = currencies.FirstOrDefault(c => c.Id == walletItem.CurrencyId);
+                walletItems.Add($"{name}: {walletItem.AmountDisplay}");
+            }
+
+            string walletValuesList = string.Join(Environment.NewLine, walletItems);
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync(walletValuesList);
+        }
+    }
+}
